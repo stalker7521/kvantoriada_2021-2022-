43b062915571abf01793cfc65cb1adc20c15f78d
30072 <it.is.mr.lag@gmail.com>
2021-07-05 10:13:50 +0500

    Has been added angel function

--- Distance.py
+++ Distance.py
@@ -2,15 +2,15 @@ from math import *
 
 
 class Distance:
-    def __init__(self, latitude1=0, longitude1=0, latitude2=0, longitude2=0, earth=6371):
+    def __init__(self, latitudeMy=0, longitudeMy=0, latitude2=0, longitude2=0, earth=6371):
         """Rad - means angel in radians, earth - means radius of Earth"""
-        self.lat1 = latitude1
-        self.lon1 = longitude1
+        self.latMy = latitudeMy
+        self.lonMy = longitudeMy
         self.lat2 = latitude2
         self.lon2 = longitude2
         self.eaR = earth
-        self.lat1Rad = radians(self.lat1)
-        self.lon1Rad = radians(self.lon1)
+        self.lat1Rad = radians(self.latMy)
+        self.lon1Rad = radians(self.lonMy)
         self.lat2Rad = radians(self.lat2)
         self.lon2Rad = radians(self.lon2)
 
@@ -23,8 +23,8 @@ class Distance:
         return length
 
     def angle(self):
-        """latitude = OY; longitude = OX (возможно надо брать в рабианах)"""
-        dtX = self.lat2 - self.lat1
-        dtY = self.lon2 - self.lon1
-        angel = dtY/dtX
+        local_lat = self.lat2 - self.latMy
+        angel = 90 - asin(local_lat/self.distance())
+        if self.lonMy > self.lon2:
+            angel *= -1
         return angel
