51c475790f96f91913ea4ed812f5f819ccc4316f
30072 <it.is.mr.lag@gmail.com>
2021-07-24 13:54:28 +0500

    add multiprocess, Arduino_send (class), Arduino has been upgraded

--- libraries and moduls/module.py
+++ libraries and moduls/module.py
@@ -4,10 +4,12 @@ import time
 import string
 import pynmea2
 from mpu9250 import *
+import multiprocessing
+import os
 
 
 class Compass:
-
+    """class for initializing local compass and taking information from it"""
     def __init__(self):
         self.mpu = MPU9250()
         self.mpu.initialize()
@@ -25,6 +27,7 @@ class Compass:
             angel = 360 + angel
         return angel
 
+##############Functions for GPS###########################################
 def my_gps():
     inf = False
     cord = ()
@@ -39,8 +42,14 @@ def my_gps():
             inf = True
     return cord
 
+
 def midl_cord(size, checker):
-    """@size - size of buffers with gps cord"""
+    """
+    function for taking midl coordinate from the range
+    :param size: int - size of this range
+    :param checker: bool - just for debug
+    :return: float, float, - midl latitude and longitude
+    """
     flag = 0
     all_lat, all_lon = [], []
     while flag != size:
@@ -50,25 +59,71 @@ def midl_cord(size, checker):
         flag += 1
         if checker:
             print(all_lat, all_lon, "flag - ", flag)
-    mid_lat = sum(all_lat)/size
-    mid_lon = sum(all_lon)/size
+    mid_lat = sum(all_lat) / size
+    mid_lon = sum(all_lon) / size
     return mid_lat, mid_lon
+########################################################################
 
-def stopper():
-    print("stop")
-    pass
+class Arduino:
+    def __init__(self, bot=9600, port='/dev/ttyACM0', timeout_1=1, dlin=4):
+        self.ser = serial.Serial(port, bot, timeout=timeout_1)
+        self.ser.flush()
+        for i in range(dlin):
+            self.ser.write('9'.encode('ascii'))
+            time.sleep(1)
 
-def moving():
-    """arduino controlling function (for start motion)"""
-    print("go")
-    pass
+    def stopper(self):
+        self.ser.write('0'.encode('ascii'))
 
-def turn():
-    """arduino controlling function (for starting rotation)"""
-    print("turning")
-    pass
+    def moving(self):
+        """arduino controlling function (for start motion)"""
+        self.ser.write('1'.encode('ascii'))
 
-def turn_zero():
-    """arduino controlling function (for returning wheels at the default state)"""
-    print("stop turning")
-    pass
+    def turn(self):
+        """arduino controlling function (for starting rotation)"""
+        self.ser.write('2'.encode('ascii'))
+
+    def turn_zero(self):
+        """arduino controlling function (for returning wheels at the default state)"""
+        self.ser.write('3'.encode('ascii'))
+
+
+class Arduino_send:
+
+    def __init__(self):
+        self.q = multiprocessing.Queue(maxsize=5)
+        self.proc = multiprocessing.Process(target=self.sender_to_ar)
+        self.proc.start()
+        time.sleep(1)
+
+    def sender_to_ar(self):
+        """sending info to Arduino"""
+        ard = Arduino(dlin=2)
+        time.sleep(1)
+        while True:
+            if self.q.empty() == False:
+                out = self.q.get()
+                if out == 'stop':
+                    ard.stopper()
+                    time.sleep(1)
+                elif out == 'go':
+                    ard.moving()
+                    time.sleep(1)
+                elif out == 'turn':
+                    ard.turn()
+                    time.sleep(1)
+                elif out == 'turn_zero':
+                    ard.turn_zero()
+                    time.sleep(1)
+
+    def sender_to_q(self, info):
+        """
+        :param info: str ('go', 'turn', 'move', 'turn_zero')
+        """
+        if self.q.empty():
+            self.q.put(info)
+
+    def killer(self):
+        """killing this process"""
+        self.proc.terminate()
+
